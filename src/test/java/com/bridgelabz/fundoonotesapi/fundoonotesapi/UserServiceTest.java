77f0e116e2276a4a92ec914a16fd92e1f6af8492
Prasad Chaudhari <pnchaudhari1996@gmail.com>
2020-12-21 16:00:31 +0530

    ADDED: Test case for user already register

--- src/test/java/com/bridgelabz/fundoonotesapi/fundoonotesapi/UserServiceTest.java
+++ src/test/java/com/bridgelabz/fundoonotesapi/fundoonotesapi/UserServiceTest.java
@@ -19,4 +19,25 @@ public class UserServiceTest {
         String message = userService.addUser(userDTO);
         Assert.assertEquals(message, "Your Account Created Successfully");
     }
+
+    @Test
+    public void givenNullUserDetails_shouldReturnUnsuccessfullyMessage(){
+        try{
+            userDTO = new UserDTO("Nikhil","","","","");
+            String message = userService.addUser(userDTO);
+            Assert.assertEquals(message, "Your Account Created Successfully");
+        } catch (FundooException e){
+            Assert.assertEquals(FundooException.ExceptionType.INVALID_DATA,e.type);
+        }
+    }
+    @Test
+    public void givenSameUserDetails_shouldReturnUserAlreadyStoreMessage(){
+        try{
+            userDTO = new UserDTO("Nikhil","Nikhil","nikhil@gmail.com","nikhil","abc");
+            String message = userService.addUser(userDTO);
+            Assert.assertEquals(message, "User Already Registered");
+        } catch (FundooException e){
+            Assert.assertEquals(FundooException.ExceptionType.INVALID_DATA,e.type);
+        }
+    }
 }
